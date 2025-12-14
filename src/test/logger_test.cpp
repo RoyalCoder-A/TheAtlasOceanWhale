76dd58a71bedfd298cac66b2fd4c88a103c45fd7
Armin Hendian <hendianarmin@gmail.com>
2025-12-14 06:27:54 +0330

    Add logger module with configuration and test cases

--- src/test/logger_test.cpp
+++ src/test/logger_test.cpp
@@ -1,3 +1,14 @@
 #include "catch2/catch_test_macros.hpp"
+#include "logger_config.hpp"
 
-TEST_CASE("1 + 1 == 2") { REQUIRE(1 + 1 == 2); }
+TEST_CASE("Logger tests") {
+    SECTION("Config tests") {
+        const auto console_config = TAOW::logger::ConsoleConfig::from_yaml(R"(
+            is_json: false
+            level: INFO
+            format: "[{}] {} => {}; {}"
+            )");
+        REQUIRE_FALSE(console_config.is_json);
+        REQUIRE(console_config.level == TAOW::logger::LogLevel::INFO);
+    }
+}
