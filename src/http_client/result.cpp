d375351c258c00471382c9c5b49f438a43201c5b
Armin Hendian <hendianarmin@gmail.com>
2025-12-28 23:00:38 +0330

    Refactor response handling by adding decompression support and optimizing split and trim functions

--- src/http_client/result.cpp
+++ src/http_client/result.cpp
@@ -1,4 +1,5 @@
 #include "taow/http_client.hpp"
+#include "taow/utils_macros.hpp"
 #include <algorithm>
 #include <cctype>
 #include <charconv>
@@ -14,8 +15,88 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
+#include <zlib.h>
 
 namespace TAOW::http_client {
+
+std::vector<std::string> split(std::string_view str, std::string_view del) {
+    std::vector<std::string> result{};
+    std::size_t start_pos{0};
+    std::size_t end_pos = str.find(del);
+    while (end_pos != std::string::npos) {
+        result.emplace_back(str.substr(start_pos, end_pos - start_pos));
+        start_pos = end_pos + del.size();
+        end_pos = str.find(del, start_pos);
+    }
+    result.emplace_back(str.substr(start_pos));
+    return result;
+}
+
+std::string trim(std::string_view str) {
+    static const std::regex re(R"(^\s+|\s+$)");
+    std::string result{};
+    std::regex_replace(std::back_inserter(result), str.begin(), str.end(), re, "");
+    return result;
+}
+
+#define COMPRESS_ALGOS_ENUM_DEFINITION(X)                                                                              \
+    X(CompressAlgos, gzip)                                                                                             \
+    X(CompressAlgos, identity)
+
+CREATE_ENUM_WITH_CASTING(CompressAlgos, COMPRESS_ALGOS_ENUM_DEFINITION);
+#undef COMPRESS_ALGOS_ENUM_DEFINITION
+
+std::vector<std::uint8_t> gunzip(const std::vector<std::uint8_t>& input) {
+    if (input.empty())
+        return {};
+
+    z_stream zs{};
+    zs.next_in = const_cast<Bytef*>(reinterpret_cast<const Bytef*>(input.data()));
+    zs.avail_in = static_cast<uInt>(input.size());
+    int rc = inflateInit2(&zs, 16 + MAX_WBITS);
+    if (rc != Z_OK)
+        throw std::runtime_error("inflateInit2 failed");
+
+    std::vector<std::uint8_t> out;
+    out.reserve(input.size() * 2);
+    std::uint8_t buffer[64 * 1024];
+    do {
+        zs.next_out = reinterpret_cast<Bytef*>(buffer);
+        zs.avail_out = static_cast<uInt>(sizeof(buffer));
+        rc = inflate(&zs, Z_NO_FLUSH);
+        if (rc != Z_OK && rc != Z_STREAM_END) {
+            inflateEnd(&zs);
+            throw std::runtime_error("inflate failed");
+        }
+        std::size_t produced = sizeof(buffer) - zs.avail_out;
+        out.insert(out.end(), buffer, buffer + produced);
+
+    } while (rc != Z_STREAM_END);
+
+    inflateEnd(&zs);
+    return out;
+}
+
+std::vector<std::uint8_t> handle_decompressing(const std::unordered_map<std::string, std::string>& headers,
+                                               const std::vector<std::uint8_t>& input) {
+    if (headers.find("content-encoding") == headers.end())
+        return input;
+    std::vector<std::uint8_t> result{input};
+    const auto encodings = split(headers.at("content-encoding"), ",");
+    for (auto i = encodings.rbegin(); i != encodings.rend(); ++i) {
+        auto encoding_enum = CompressAlgos_from_string(trim(*i));
+        if (!encoding_enum)
+            throw std::runtime_error("Can't decompress " + *i);
+        switch (encoding_enum.value()) {
+        case gzip:
+            result = gunzip(result);
+            break;
+        case identity:
+            break;
+        }
+    }
+    return result;
+}
 
 std::vector<std::uint8_t> process_chunked_response(const std::vector<uint8_t>& body_bytes) {
     const std::string_view line_separator = "\r\n";
@@ -42,26 +123,6 @@ std::vector<std::uint8_t> process_chunked_response(const std::vector<uint8_t>& b
         result.insert(result.end(), data_part_beginning, data_part_beginning + chunk_size);
         sol = data_part_beginning + chunk_size + line_separator.size();
     }
-    return result;
-}
-
-std::vector<std::string> split(std::string_view str, std::string_view del) {
-    std::vector<std::string> result{};
-    std::size_t start_pos{0};
-    std::size_t end_pos = str.find(del);
-    while (end_pos != std::string::npos) {
-        result.emplace_back(str.substr(start_pos, end_pos - start_pos));
-        start_pos = end_pos + del.size();
-        end_pos = str.find(del, start_pos);
-    }
-    result.emplace_back(str.substr(start_pos));
-    return result;
-}
-
-std::string trim(std::string_view str) {
-    static const std::regex re(R"(^\s+|\s+$)");
-    std::string result{};
-    std::regex_replace(std::back_inserter(result), str.begin(), str.end(), re, "");
     return result;
 }
 
@@ -111,11 +172,12 @@ Response Response::from_raw_bytes(const std::vector<std::uint8_t>& raw_bytes) {
 }
 
 std::vector<std::uint8_t> Response::body_bytes() const {
+    auto result = this->_body_bytes;
     if (const auto founded = this->_response_header.find("transfer-encoding");
         founded != _response_header.end() && founded->second.find("chunked") != std::string::npos) {
-        return process_chunked_response(this->_body_bytes);
+        result = process_chunked_response(result);
     }
-    return this->_body_bytes;
+    return handle_decompressing(this->_response_header, result);
 }
 
 std::string Response::body_text() const {
