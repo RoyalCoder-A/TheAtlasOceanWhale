38f224177dbb4bf5db19b82f1375224e372d0f5a
Armin Hendian <hendianarmin@gmail.com>
2026-01-01 23:01:49 +0000

    Add Conan configuration and update CMake files for dependencies management

---
+++ conanfile.py
@@ -0,0 +1,16 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class TAOWRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("catch2/3.12.0")
+        self.requires("nlohmann_json/3.12.0")
+        self.requires("boost/1.90.0")
+        self.requires("zlib/1.3.1")
+
+    def layout(self):
+        cmake_layout(self)
